bfe07171f07792357973b52bc8817a97689d62ec
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-05 05:57:29 +0200

    Active navlink added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
     return (
@@ -11,14 +12,14 @@ const NavBar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><NavLink to='/'>Homepage</NavLink></li>
-                        <li><NavLink to="/contact">Contact</NavLink></li>
-                        <li><NavLink to='/about' >About</NavLink></li>
+                        <li><NavLink className={({isActive,isPending})=> isActive ? "active" : isPending ? "pending" : ""} to='/'>Homepage</NavLink></li>
+                        <li><NavLink className={({isActive})=> isActive ? "active" : ""} to="/contact">Contact</NavLink></li>
+                        <li><NavLink className={({isActive})=> isActive ? "active" : ""} to='/about' >About</NavLink></li>
                     </ul>
                     </div>
                 </div>
                 <div className="navbar-center">
-                    <NavLink className="btn btn-ghost normal-case text-xl">daisyUI</NavLink>
+                    <NavLink className={({isActive})=> isActive ? "btn btn-ghost normal-case text-xl text-cyan-600" : ""} to='/'>Simple Blog</NavLink>
                 </div>
                 <div className="navbar-end">
                     <button className="btn btn-ghost btn-circle">
